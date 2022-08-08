4dd0eb106820fbc706f6c7b79a34940474ed53c9
William Goulet <willgoulet555@gmail.com>
2022-08-07 20:47:12 -0400

    Add email and contact form

--- js/script.js
+++ js/script.js
@@ -12,3 +12,31 @@ window.onload = function () {
     });
 
 }
+
+$(document).on('click', '#btnContactUs', function(){
+    var em = $("#email").val();
+    var na = $("#name").val();
+    var msg = $("#message").val();
+
+
+    var dataString = 'email=' + em + '&name=' + na + '&msg=' + msg;
+    if(em==""||na==""||msg=="")
+    {
+        $("#display").html("<div class='alert alert-warning'>Veuillez compléter tous les champs.</div>");
+    }
+    else
+    {
+        Email.send({
+            Host : "smtp.elasticemail.com",
+            Username : "epoxysjdmail@gmail.com",
+            Password : "45C461DA9D4FD4DA53678CD025C0C2790CC2",
+            To : "willgoulet555@gmail.com",
+            From : "epoxysjdmail@gmail.com",
+            Subject : "subject",
+            Body : "body"
+        }).then(
+          message => alert(message)
+        );
+    }
+    return false;
+  });
