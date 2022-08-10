c1fdc4442f34e3d618ece374f982309a966dc0df
William Goulet <willgoulet555@gmail.com>
2022-08-09 21:36:32 -0400

    remove phpmailer, custom contact section

--- js/script.js
+++ js/script.js
@@ -11,51 +11,9 @@ window.onload = function () {
       $('.navbar-collapse').collapse('hide');
     });
 
+   
+
 }
 
-/*$(document).on('click', '#btnContactUs', function(){
-    var em = $("#email").val();
-    var na = $("#name").val();
-    var msg = $("#message").val();
 
 
-    var dataString = 'email=' + em + '&name=' + na + '&msg=' + msg;
-    if(em==""||na==""||msg=="")
-    {
-        $("#display").html("<div class='alert alert-warning'>Veuillez compléter tous les champs.</div>");
-    }
-    else
-    {
-        Email.send({
-            Host : "smtp.elasticemail.com",
-            Username : "epoxysjdmail@gmail.com",
-            Password : "988C58757369BE7B208B583701816EA3292E",
-            To : "willgoulet555@gmail.com",
-            From : "epoxysjdmail@gmail.com",
-            Subject : "subject",
-            Body : "body"
-        }).then(
-          message => alert(message)
-        );
-    }
-    return false;
-  });*/
-
-/*$(document).on('click', '#btnContactUs', function(){
-    $.ajax({
-    type: "POST",
-    url: "http://localhost/mail.php",
-    data: { name: "John" }
-    }).done(function( ) {
-      alert("test'");
-    });
-});*/
-
-
-  /*$.ajax({
-    type: "POST",
-    url: "mail.php",
-    data: { name: "John" }
-  }).done(function( ) {
-      alert('test');
-  });*/
