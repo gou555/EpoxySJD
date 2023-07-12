869e7345aa0674d6abea4d3815a68f4702c0c69a
William Goulet <willgoulet555@gmail.com>
2023-07-12 13:03:00 -0400

    test

--- js/script.js
+++ js/script.js
@@ -10,10 +10,33 @@ window.onload = function () {
     element.addEventListener("click", function (event) {
       $('.navbar-collapse').collapse('hide');
     });
-
-   
-
+    
+    $("#close-btn").click(function(){
+        // remove active class from all images
+       $(".small-image").removeClass('active');
+       $("#show_image_popup").slideUp();
+     })
+ 
+     $(".small-image").click(function(){
+         // remove active class from all images
+        $(".small-image").removeClass('active');
+       // add active class
+        $(this).addClass('active');
+ 
+       var image_path = $(this).attr('src'); 
+       $("#show_image_popup").fadeOut();
+       // now st this path to our popup image src
+       $("#show_image_popup").fadeIn();
+       $("#large-image").attr('src',image_path);
+ 
+     })
+    
 }
+
+
+
+
+
 
 
 
